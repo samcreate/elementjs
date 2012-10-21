46bf42e55f54a1bf0e9a138ef56a42398ed704b0
samcreate <aaron@evb.com>
2012-10-20 20:02:10 -0700

    adding trailing slash to js assets

--- www/lib/php/includes/html_footer.php
+++ www/lib/php/includes/html_footer.php
@@ -9,13 +9,13 @@
 	<![endif]-->
 	
 	<!-- BEGIN <?php echo $settings->environment ?>: javascript -->
-	<script src="lib/js/jquery/jquery-1.8.0.min.js" type="text/javascript" charset="utf-8"></script>
-	<script type="text/javascript" src="lib/js/plugins/shCore.js"></script>
-	<script src="lib/js/plugins/shBrushJScript.js" type="text/javascript" charset="utf-8"></script>
-	<script src="lib/js/plugins/Element.js" type="text/javascript" charset="utf-8"></script>
-	<script src="lib/js/master.js" type="text/javascript" charset="utf-8"></script>	
-	<script src="lib/js/main.js" type="text/javascript" charset="utf-8"></script>
-	<script src="lib/js/homePage.js" type="text/javascript" charset="utf-8"></script>
+	<script src="/lib/js/jquery/jquery-1.8.0.min.js" type="text/javascript" charset="utf-8"></script>
+	<script type="text/javascript" src="/lib/js/plugins/shCore.js"></script>
+	<script src="/lib/js/plugins/shBrushJScript.js" type="text/javascript" charset="utf-8"></script>
+	<script src="/lib/js/plugins/Element.js" type="text/javascript" charset="utf-8"></script>
+	<script src="/lib/js/master.js" type="text/javascript" charset="utf-8"></script>	
+	<script src="/lib/js/main.js" type="text/javascript" charset="utf-8"></script>
+	<script src="/lib/js/homePage.js" type="text/javascript" charset="utf-8"></script>
 	<!-- END: javascript -->
 
 </body>
