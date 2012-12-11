4141ed1acfdf85ade18db1503b5c0cb5ac8733bf
samcreate <aaron@evb.com>
2012-12-10 22:41:40 -0800

    fixing issues with performace

--- www/sandbox/lib/js/Element/FrameTicker.js
+++ www/sandbox/lib/js/Element/FrameTicker.js
@@ -32,17 +32,48 @@
 		if (this.timerID) return;
 		var _this = this;
 		(function() {
-				_this.fire("tick");
-				// if(_this.on) _this.timerID = setTimeout(arguments.callee, p_framerate);
-				if(_this.on) _this.timerID = window.requestAnimFrame(arguments.callee);
+				
+				var _scrope = arguments.callee;
+				if(_this.on){
+					_this._requestTimeout(function(){
+						_this.fire("tick");
+						_this.timerID = window.requestAnimFrame(_scrope);
+					}, p_framerate);
+					
+				} 
 
 
 		})();
 		return _this;
 	};
+
 	
 	_pt.stop = function(){
 		this.on = false;
+	};
+
+	_pt._requestTimeout = function(fn, delay){
+
+		if( !window.requestAnimationFrame      	&& 
+		!window.webkitRequestAnimationFrame && 
+		!(window.mozRequestAnimationFrame && window.mozCancelRequestAnimationFrame) && // Firefox 5 ships without cancel support
+		!window.oRequestAnimationFrame      && 
+		!window.msRequestAnimationFrame)
+			return window.setTimeout(fn, delay);
+
+		var start = new Date().getTime(),
+			handle = new Object();
+
+		function loop(){
+			var current = new Date().getTime(),
+				delta = current - start;
+
+			delta >= delay ? fn.call() : handle.value = requestAnimFrame(loop);
+		};
+
+		handle.value = requestAnimFrame(loop);
+		return handle;
+
 	};
 	
 
