60f5bf18eeb1d3bef327aec977c368a0476a549d
Aaron McGuire <aaron.mcguire@evb.com>
2013-03-19 20:23:52 -0700

    test

--- www/lib/php/includes/html_footer.php
+++ www/lib/php/includes/html_footer.php
@@ -18,7 +18,16 @@
 	<script src="/lib/js/homePage.js" type="text/javascript" charset="utf-8"></script>
 	<!-- END: javascript -->
 	
-
-	
+	<script type="text/javascript">
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-39447718-1']);
+      _gaq.push(['_trackPageview']);
+    
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+  </script>
 </body>
 </html>
