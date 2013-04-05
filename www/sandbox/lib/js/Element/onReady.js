e27a1c90d05ce3f7915c328f76ab09db4cf675eb
Aaron McGuire <aaron.mcguire@evb.com>
2013-04-04 23:23:07 -0700

    cleaning up for group dev

--- www/sandbox/lib/js/Element/onReady.js
+++
@@ -1,9 +0,0 @@
-var onReady = function(obj){
-	if (!document.createElement('canvas').getContext) {
-		//no work
-		obj.error({event:'error',info:"Canvas support was not detected."});
-	}else{
-		//it works
-		obj.success({event:'success',info:"Canvas support detected!"})
-	}
-}
