c481f396807c1110ee9648edc1fb1ed0648219e5
Aaron McGuire <aaron.mcguire@evb.com>
2013-04-04 23:36:55 -0700

    adding marjor fixes to groups and other refactoring

--- build/build.js
+++ build/build.js
@@ -4,25 +4,26 @@
  */
 
 /* Put all major classes here for build. Main.js is required at the top of the stack.
-* @depends ../www/sandbox/lib/js/Element/util.js
 * @depends ../www/sandbox/lib/js/Element/Basic.js
-* @depends ../www/sandbox/lib/js/Element/Animate.js
+* @depends ../www/sandbox/lib/js/Element/util.js
 * @depends ../www/sandbox/lib/js/Element/EventTarget.js
+* @depends ../www/sandbox/lib/js/Element/types/Scene.js
+* @depends ../www/sandbox/lib/js/Element/Animate.js
 * @depends ../www/sandbox/lib/js/Element/Filter.js
 * @depends ../www/sandbox/lib/js/Element/DisplayObject.js
-* @depends ../www/sandbox/lib/js/Element/Loop.js
 * @depends ../www/sandbox/lib/js/Element/Transform.js
 * @depends ../www/sandbox/lib/js/Element/Events.js
+* @depends ../www/sandbox/lib/js/Element/CanvasInteract.js
 * @depends ../www/sandbox/lib/js/Element/types/Graphics.js
+* @depends ../www/sandbox/lib/js/Element/types/Group.js
 * @depends ../www/sandbox/lib/js/Element/types/Bitmap.js
+* @depends ../www/sandbox/lib/js/Element/types/Shape.js
 * @depends ../www/sandbox/lib/js/Element/types/Text.js
 * @depends ../www/sandbox/lib/js/Element/types/Path.js
 * @depends ../www/sandbox/lib/js/Element/types/Sprite.js
-* @depends ../www/sandbox/lib/js/Element/types/Shape.js
 * @depends ../www/sandbox/lib/js/Element/types/Rectangle.js
 * @depends ../www/sandbox/lib/js/Element/FrameTicker.js
 * @depends ../www/sandbox/lib/js/Element/Tweenie.js
-* @depends ../www/sandbox/lib/js/Element/canvas.js
 * @depends ../www/sandbox/lib/js/Element/element.js
  */
 
