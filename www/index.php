2f2831e59f77d7df0c16ee61a49051a376ce66c0
Aaron McGuire <aaron.mcguire@evb.com>
2013-04-11 11:50:51 -0700

    more fixes

--- www/index.php
+++ www/index.php
@@ -13,38 +13,48 @@
 
 							<script src="element.js" type="text/javascript" charset="utf-8"></script>
 
-							//Include Element.js to your document.
+							// Include Element.js to your document.
 						</pre> -->
-						<div><div id="highlighter_707907" class="syntaxhighlighter nogutter  js"><div class="toolbar"><span><a href="#" class="toolbar_item command_help help">?</a></span></div><table border="0" cellpadding="0" cellspacing="0"><tbody><tr><td class="code"><div class="container"><div class="line number1 index0 alt2"><code class="js lightyellowish">&lt;</code><code class="js yellowish">script</code> <code class="js lightyellowish">src=</code><code class="js string">"element.js"</code> <code class="js lightyellowish">type=</code><code class="js string">"text/javascript"</code> <code class="js lightyellowish">charset=</code><code class="js string">"utf-8"</code><code class="js lightyellowish">&gt;</code><code class="js lightyellowish">&lt;</code><code class="js plain">/</code><code class="js yellowish">script</code><code class="js lightyellowish">&gt;</code></div><div class="line number2 index1 alt1">&nbsp;</div><div class="line number3 index2 alt2"><code class="js comments">//Include Element.js to your document.</code></div></div></td></tr></tbody></table></div></div>
+						<div class="syntaxhighlighter nogutter  js" id="highlighter_797568"><div class="toolbar"><span><a class="toolbar_item command_help help" href="#">?</a></span></div><table cellspacing="0" cellpadding="0" border="0"><tbody><tr><td class="code"><div class="container"><div class="line number1 index0 alt2"><code class="js lightyellowish">&lt;</code><code class="js yellowish">script</code> <code class="js lightyellowish">src=</code><code class="js string">"element.js"</code> <code class="js lightyellowish">type=</code><code class="js string">"text/javascript"</code> <code class="js lightyellowish">charset=</code><code class="js string">"utf-8"</code><code class="js lightyellowish">&gt;</code><code class="js lightyellowish">&lt;</code><code class="js plain">/</code><code class="js yellowish">script</code><code class="js lightyellowish">&gt;</code></div><div class="line number2 index1 alt1">&nbsp;</div><div class="line number3 index2 alt2"><code class="js comments">// Include Element.js to your document.</code></div></div></td></tr></tbody></table></div>
 					</dd>
 	
 				<dt class="fire">Step 2:</dt>
 					<dd>
 						<!-- <pre class="brush: js; gutter: false; ">
 
-							<canvas id="element-root" framerate="10"> No support for canvas was detected, human. </canvas>
-
-							//First step is to add a canvas element to your page with an id of “element-root”.
-							//For framerate, height and width of your project is all based on the “meta-data” 
-							//that you add to the canvas element.
+							<canvas id="scene1"> No support for canvas was detected, human. </canvas>
+							// Add a canvas element to the page with a unique ID.
 						</pre> -->
-						<div><div id="highlighter_28919" class="syntaxhighlighter nogutter  js"><div class="toolbar"><span><a href="#" class="toolbar_item command_help help">?</a></span></div><table border="0" cellpadding="0" cellspacing="0"><tbody><tr><td class="code"><div class="container"><div class="line number1 index0 alt2"><code class="js lightyellowish">&lt;</code><code class="js yellowish">canvas</code> <code class="js lightyellowish">id=</code><code class="js string">"element-root"</code> <code class="js lightyellowish">framerate=</code><code class="js string">"10"</code> <code class="js blue">width</code><code class="js plain">=</code><code class="js string">"1279"</code> <code class="js blue">height</code><code class="js plain">=</code><code class="js string">"1385"</code><code class="js lightyellowish">&gt;</code> <code class="js offwhite">No support for canvas was detected, human.</code><code class="js lightyellowish">&lt;</code><code class="js plain">/</code><code class="js yellowish">canvas</code><code class="js lightyellowish">&gt;</code></div><div class="line number2 index1 alt1">&nbsp;</div><div class="line number3 index2 alt2"><code class="js comments">//First step is to add a canvas element to your page with an id of “element-root”.</code></div><div class="line number4 index3 alt1"><code class="js comments">//For framerate, height and width of your project is all based on the “meta-data” </code></div><div class="line number5 index4 alt2"><code class="js comments">//that you add to the canvas element.</code></div></div></td></tr></tbody></table></div></div>
+						<div class="syntaxhighlighter nogutter  js" id="highlighter_370060"><div class="toolbar"><span><a class="toolbar_item command_help help" href="#">?</a></span></div><table cellspacing="0" cellpadding="0" border="0"><tbody><tr><td class="code"><div class="container"><div class="line number1 index0 alt2"><code class="js lightyellowish">&lt;</code><code class="js yellowish">canvas</code> <code class="js lightyellowish">id=</code><code class="js string">"scene1"</code><code class="js lightyellowish">&gt;</code> <code class="js offwhite">No support for canvas was detected, human.</code> <code class="js lightyellowish">&lt;</code><code class="js plain">/</code><code class="js yellowish">canvas</code><code class="js lightyellowish">&gt;</code></div><div class="line number2 index1 alt1"><code class="js comments">// Add a canvas element to the page with a unique ID.</code></div></div></td></tr></tbody></table></div>
 					</dd>
 	
 				<dt class="atom">Step 3:</dt>
 					<dd>
 						<!-- <pre class="brush: js; gutter: false; ">
 
+							// Create a new Scene and pass in a object with the canvas id, framerate, width and height.
+
+							var Scene1 = new Element("Scene",{
+								canvas:'scene1',
+								framerate: 80,
+								width: 740,
+								height:480
+							});
+
+							
+							// Next, create a Sprite element and pass in our png sprite and json file 
+							// (created with texture packer)
+
 							var fire = new Element("Sprite",{
 								src  : "fire.png, fire.json",
 								loop : true
 							});
 
-							//Create a new Elment by defining what the Element type is. In the above example
-							//we have a new Sprite Element being created and automatically added to the canvas.
-							//We define the Element further with “src, loop, x, y" by passing them as an object.
+							Scene1.add(fire);
+							
+							// Next, read the documentation!	
 						</pre> -->
-						<div><div id="highlighter_453298" class="syntaxhighlighter nogutter  js"><div class="toolbar"><span><a href="#" class="toolbar_item command_help help">?</a></span></div><table border="0" cellpadding="0" cellspacing="0"><tbody><tr><td class="code"><div class="container"><div class="line number1 index0 alt2"><code class="js offwhite">var</code> <code class="js plain">fire = </code><code class="js keyword">new</code> <code class="js yellowish">Element</code><code class="js plain">(</code><code class="js string">"Sprite"</code><code class="js plain">,{</code></div><div class="line number2 index1 alt1"><code class="js spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code class="js blue">src</code>&nbsp; <code class="js plain">: </code><code class="js string">"fire.png, fire.json"</code><code class="js plain">,</code></div><div class="line number3 index2 alt2"><code class="js spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code class="js blue">loop</code> <code class="js plain">: </code><code class="js keyword">true</code></div><div class="line number4 index3 alt1"><code class="js plain">});</code></div><div class="line number5 index4 alt2">&nbsp;</div><div class="line number6 index5 alt1"><code class="js comments">//Create a new Elment by defining what the Element type is. In the above example</code></div><div class="line number7 index6 alt2"><code class="js comments">//we have a new Sprite Element being created and automatically added to the canvas.</code></div><div class="line number8 index7 alt1"><code class="js comments">//We define the Element further with “src, loop, x, y" by passing them as an object.</code></div></div></td></tr></tbody></table></div></div>
+						<div class="syntaxhighlighter nogutter  js" id="highlighter_726876"><div class="toolbar"><span><a class="toolbar_item command_help help" href="#">?</a></span></div><table cellspacing="0" cellpadding="0" border="0"><tbody><tr><td class="code"><div class="container"><div class="line number1 index0 alt2"><code class="js comments">// Create a new Scene and pass in a object with the canvas id, framerate, width and height.</code></div><div class="line number2 index1 alt1">&nbsp;</div><div class="line number3 index2 alt2"><code class="js offwhite">var</code> <code class="js plain">Scene1 = </code><code class="js keyword">new</code> <code class="js yellowish">Element</code><code class="js plain">(</code><code class="js string">"Scene"</code><code class="js plain">,{</code></div><div class="line number4 index3 alt1"><code class="js spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code class="js yellowish">canvas</code><code class="js plain">:</code><code class="js string">'scene1'</code><code class="js plain">,</code></div><div class="line number5 index4 alt2"><code class="js spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code class="js plain">framerate: </code><code class="js blue">80</code><code class="js plain">,</code></div><div class="line number6 index5 alt1"><code class="js spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code class="js blue">width</code><code class="js plain">: </code><code class="js blue">740</code><code class="js plain">,</code></div><div class="line number7 index6 alt2"><code class="js spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code class="js blue">height</code><code class="js plain">:</code><code class="js blue">480</code></div><div class="line number8 index7 alt1"><code class="js plain">});</code></div><div class="line number9 index8 alt2">&nbsp;</div><div class="line number10 index9 alt1">&nbsp;</div><div class="line number11 index10 alt2"><code class="js comments">// Next, create a Sprite element and pass in our png sprite and json file </code></div><div class="line number12 index11 alt1"><code class="js comments">// (created with texture packer)</code></div><div class="line number13 index12 alt2">&nbsp;</div><div class="line number14 index13 alt1"><code class="js offwhite">var</code> <code class="js plain">fire = </code><code class="js keyword">new</code> <code class="js yellowish">Element</code><code class="js plain">(</code><code class="js string">"Sprite"</code><code class="js plain">,{</code></div><div class="line number15 index14 alt2"><code class="js spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code class="js blue">src</code>&nbsp; <code class="js plain">: </code><code class="js string">"fire.png, fire.json"</code><code class="js plain">,</code></div><div class="line number16 index15 alt1"><code class="js spaces">&nbsp;&nbsp;&nbsp;&nbsp;</code><code class="js blue">loop</code> <code class="js plain">: </code><code class="js keyword">true</code></div><div class="line number17 index16 alt2"><code class="js plain">});</code></div><div class="line number18 index17 alt1">&nbsp;</div><div class="line number19 index18 alt2"><code class="js plain">Scene1.add(fire);</code></div><div class="line number20 index19 alt1">&nbsp;</div><div class="line number21 index20 alt2"><code class="js comments">// Next, read the documentation!&nbsp;&nbsp;&nbsp; </code></div></div></td></tr></tbody></table></div>
 					</dd>
 				<dt class="light">Step 3:</dt>
 					<dd>
